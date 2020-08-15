5c7a61e43ea41de74e610ff6b602b73432c69161
msksaul <64514098+msksaul@users.noreply.github.com>
2020-08-15 16:11:01 -0600

    Setup React Router

--- src/App.js
+++ src/App.js
@@ -1,11 +1,24 @@
 import React from 'react';
 import './App.css';
+import { BrowserRouter as Router , Switch, Route } from 'react-router-dom';
 
 function App() {
   return (
-    <div className="App">
-      <h1>Hola</h1>
+    <Router>
+    <div className="app">
+      <Switch>
+        <Route path='/checkout'>
+          <h1>Checkout</h1>
+        </Route>
+        <Route path='/login'>
+          <h1>Login Page</h1>
+        </Route>
+        <Route path='/'>
+          <h1>HOME PAGE</h1>
+        </Route>
+      </Switch>
     </div>
+    </Router>
   );
 }
 
