579af3bf3378ddf6eaaea0cbe888305cca416a60
msksaul <64514098+msksaul@users.noreply.github.com>
2020-08-18 17:59:17 -0600

    Subtotal Component

--- src/components/Checkout.js
+++ src/components/Checkout.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { useStateValue } from '../StateProvider';
 import CheckoutProduct from '../components/CheckoutProduct';
+import Subtotal from '../components/Subtotal';
 import './Checkout.css';
 
 function Checkout() {
@@ -9,8 +10,9 @@ function Checkout() {
 
   return(
     <div className='checkout'>
+      <div className='checkout__left'>
       <img 
-        className='checkout__add' 
+        className='checkout__ad' 
         src='https://images-na.ssl-images-amazon.com/images/G/02/UK_CCMP/TM/OCC_Amazon1._CB423492668_.jpg' 
         alt='checkout'/>
       {basket?.length === 0 ? (
@@ -34,6 +36,12 @@ function Checkout() {
               rating={item.rating}
             />
           ))} 
+        </div>
+      )}
+      </div>
+      {basket.length > 0 && (
+        <div className='checkout__right'>
+          <Subtotal/>
         </div>
       )}
     </div>
